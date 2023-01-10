b7eb9ff09cb547eee54bbcc0b8828e009b432946
Neceva <a.neceva@gmail.com>
2023-01-10 16:27:11 +0200

    Exercise: Basic Syntax, Conditional Statements and Loops
    This is a  solutions of problems in  Exercise: Basic Syntax, Conditional Statements and Loops.

---
+++ exercise_basic_syntax_conditional_statements_and_loops/chat_codes.py
@@ -0,0 +1,12 @@
+number_of_messages = int(input())
+for i in range(number_of_messages):
+    number = int(input())
+    if number == 88:
+        print("Hello")
+    elif number == 86:
+        print("How are you?")
+    elif number < 88:
+        print("GREAT!")
+    elif number > 88:
+        print("Bye")
+
---
+++ exercise_basic_syntax_conditional_statements_and_loops/jenny's_secret_essage.py
@@ -0,0 +1,5 @@
+input_name = input()
+if input_name == "Johnny":
+    print("Hello, my love!")
+else:
+    print(f"Hello, {input_name}!")
