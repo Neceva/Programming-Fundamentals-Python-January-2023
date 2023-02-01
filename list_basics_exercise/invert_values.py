1b5ac4913179d682733170c7199b5417a0a720b3
Anna-Mariya Netseva-Borikova <a.neceva@gmail.com>
2023-02-01 14:30:37 +0200

    add new tasks

---
+++ list_basics_exercise/invert_values.py
@@ -0,0 +1,7 @@
+user_input = input().split()
+final_list = []
+for num in user_input:
+    number = -int(num)
+    final_list.append(number)
+
+print(final_list)
---
+++ list_basics_exercise/multiples_list.py
@@ -0,0 +1,7 @@
+factor = int(input())
+count = int(input())
+lst_num = []
+for i in range(1, count + 1):
+    lst_num.append(factor * i)
+
+print(lst_num)
